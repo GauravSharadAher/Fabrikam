63417e148e5439e2264c95eb84f72d902ea753b9
Gaurav Sharad Aher <114145131+GauravSharadAher@users.noreply.github.com>
2023-08-24 23:51:00 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,31 @@
+
+#User_login 
+
+insert into user_login values(default,"gauravaher@gmail.com","1234","student","Gaurav");
+insert into user_login values(default,"ashishmahajan01@gmail.com","1234","student","Ashish");
+insert into user_login values(default,"ruturaj@gmail.com","1234","HR","Ruturaj");
+insert into user_login values(default,"deepak@gmail.com","1234","College","Deepak");
+insert into user_login values(default,"sujit@gmail.com","1234","College","Sujit");
+
+
+#university
+
+insert into university values (default,"Ganeshkhind Rd, Ganeshkhind, Pune, Maharashtra 411007","Pune","Savitribai Phule Pune University, formerly the University of Pune, is a public state university located in the city of Pune.The university houses 46 academic departments.",null,"Savitribai Phule Pune University",10,"Maharashtra");
+
+insert into university values (default,"Mahatma Gandhi Road, Mantralaya, Fort, Mumbai, Maharashtra 400032","Mumbai","The University of Mumbai is a public state university in Mumbai. It is one of the largest university systems in the world with over 549,000 students on its campuses and affiliated colleges.",null,"University of Mumbai",56,"Maharashtra");
+
+insert into university values (default,"Main Gate Rd, IIT Area, Powai, Mumbai, Maharashtra 400076","Mumbai"," IIT Bombay is the topmost preferred choice for Indian students in STEM fields such as computer science and engineering.",null,"Indian Institute of Technology Bombay",1,"Maharashtra");
+
+insert into university values (default,"Bharati Vidyapeeth Bhavan, Lal Bahadur Shastri Rd, Joshi Wada, Sadashiv Peth, Pune, Maharashtra 411030","Pune","Bharati Vidyapeeth is a 58 year old private deemed to be university established in Pune, India.",null,"Bharati Vidyapeeth Deemed University",76,"Maharashtra");
+
+insert into university values (default,"43GW+CR2, Hanuman Nagar, Ajni Rd, Medical Chowk, Ajni, Nagpur, Maharashtra 440003","Nagpur","Government Medical College and Hospital, Nagpur is a medical college located in Ajni which is part of South Nagpur, Maharashtra, India",null,"Govt Medical College, Nagpur",62,"Maharashtra");
+
+
+#college predictor
+
+insert into college_predictor values (default,"JEE",60,62.2,68.2,60.3,64.6,null,null,null);
+insert into college_predictor values (default,"JEE",80,84.2,78.2,81.3,84.6,null,null,null);
+insert into college_predictor values (default,"NEET",680,684,640,604,600,null,null,null);
+insert into college_predictor values (default,"12th percentage",65,64.2,62.2,61.3,68.6,null,null,null);
+insert into college_predictor values (default,"CET",,80,84.2,78.2,81.3,84.6,null,null,null);
+
